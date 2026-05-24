7ee12f6a929c8030218337949c2414dd5148c9bc
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2026-05-24 22:41:47 +0500

    changing in rotes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,6 @@ import { Router } from "express";
 import transectionRouter from "../module/transection/routes.js";
 
 const router = Router()
-router.get("/", (req, res) => {
-    res.send("Expense Tracker API Working...");
-});
-
 router.use('/transection', transectionRouter)
 
 export default router
