437c9aa7ed58c743fcefe44ea739e19ae0a7faf5
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2026-05-25 19:55:01 +0500

    remove localhost path inside of the cors

--- index.js
+++ index.js
@@ -7,15 +7,9 @@ dotenv.config();
 connectDB();
 import cors from "cors";
 
-
 const app = express();
 app.use(express.json());
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Expense Tracker Backend Running...");
